a5db4b2cdec3e58cc8071592049ed8d5322958af
rafaeelferreir4 <rafaelferreira2001@live.com>
2019-04-20 22:30:56 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import "./style.css";
+class Header extends Component {
+    render() {
+        return <header id="main-header">JsHunt</header>
+    }
+}
+
+export default Header;
